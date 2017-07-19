ad2ed390528edcee814aa7803e1b8a04ab301444
Jun <lleellee013@gmail.com>
2017-07-19 23:05:38 +0900

    downloadMp4 API 생성 - API 형태 : mp4.download(url, downloadPath, fileName);
    추후 할일 downloadMp3 API 생성

---
+++ app.js
@@ -0,0 +1,3 @@
+const mp4 = require('./downloadMp4');
+
+mp4.download('https://www.youtube.com/watch?v=afxLaQiLu-o&index=2&list=PLUV2GDi6R7N_o0GjarrD6_DsyA8IB_FuK', 'C:\\Users\\jun\\Desktop\\in_develope\\youtube-downloader\\download', '헤이즈.mp4');
