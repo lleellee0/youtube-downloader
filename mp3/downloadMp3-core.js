93022f50d5d92be78e45350994e5734f1b9eaafd
Jun <lleellee013@gmail.com>
2017-07-29 19:30:49 +0900

    ffmpeg.exe 파일 내장 (for windows)
    1. app.js에 기본으로 내장된 api 테스트용 파일을 developers 로 대체
    2. issue.txt 파일에 각종 이슈 및 해결 방법 기록

---
+++ mp3/downloadMp3-core.js
@@ -0,0 +1,54 @@
+var YoutubeMp3Downloader = require("youtube-mp3-downloader");
+
+const fs = require("fs");
+const conf = JSON.parse(fs.readFileSync('conf/conf.json'));
+
+var Downloader = function(outputPath) {
+    var self = this;
+
+    //Configure YoutubeMp3Downloader with your settings
+    self.YD = new YoutubeMp3Downloader({
+        "ffmpegPath": conf.ffmpegPath,        // Where is the FFmpeg binary located?
+        "outputPath": outputPath,    // Where should the downloaded and encoded files be stored?
+        "youtubeVideoQuality": "highest",       // What video quality should be used?
+        "queueParallelism": 2,                  // How many parallel downloads/encodes should be started?
+        "progressTimeout": 2000                 // How long should be the interval of the progress reports
+    });
+
+    self.callbacks = {};
+
+    self.YD.on("finished", function(error, data) {
+
+        if (self.callbacks[data.videoId]) {
+            self.callbacks[data.videoId](error, data);
+        } else {
+            console.log("Error: No callback for videoId!");
+        }
+
+    });
+
+    self.YD.on("error", function(error, data) {
+        console.error(error + " on videoId " + data.videoId);
+
+        if (self.callbacks[data.videoId]) {
+            self.callbacks[data.videoId](error, data);
+        } else {
+            console.log("Error: No callback for videoId!");
+        }
+
+    });
+
+};
+
+Downloader.prototype.getMP3 = function(track, callback){
+
+    var self = this;
+
+    // Register callback
+    self.callbacks[track.videoId] = callback;
+    // Trigger download
+    self.YD.download(track.videoId, track.name);
+
+};
+
+module.exports = Downloader;
