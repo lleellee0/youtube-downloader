e9bbc21c120dadc876a297c005a0ab118032a7ce
Jun <lleellee013@gmail.com>
2017-09-10 16:36:47 +0900

    경로 오류 수정
    youtube-downloader-api => fyda

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const mp3 = require('./mp3/downloadMp3');
 const mp4 = require('./mp4/downloadMp4');
 
-
-// mp3.download('https://www.youtube.com/watch?v=Vhh_GeBPOhs',
-// 'C:\\Users\\jun\\Desktop\\in_develope\\youtube-downloader\\download', 'developers.mp3');
-
-// mp4.download('https://www.youtube.com/watch?v=Vhh_GeBPOhs',
-//  'C:\\Users\\jun\\Desktop\\in_develope\\youtube-downloader\\download', 'developers.mp4');
-
 exports.downloadMp3 = mp3.download;
 exports.downloadMp4 = mp4.download;
+
+
+mp4.download('https://www.youtube.com/watch?v=OzziXjV4rDo', '.', '타짜.mp4');
+
+// Use case
+// const fyda = require('fyda');
+
+// fyda.downloadMp3('https://www.youtube.com/watch?v=Vhh_GeBPOhs','C:\\Users\\jun\\Desktop\\in_develope\\youtube-downloader\\download', 'developers.mp3');
+// fyda.downloadMp4('https://www.youtube.com/watch?v=Vhh_GeBPOhs','C:\\Users\\jun\\Desktop\\in_develope\\youtube-downloader\\download', 'developers.mp4');
