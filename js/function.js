d8518f346f973fffdc1672f42a8295995acc843a
lleellee0 <lleellee013@gmail.com>
2019-03-04 03:19:28 +0900

    재생목록 URL 접근 성공

--- js/function.js
+++ js/function.js
@@ -1,5 +1,7 @@
 const log = require('./js/log.js');
 const fs = require('fs');
+const request = require('request');
+const cheerio = require('cheerio');
 
 // Add Event Lintener
 let inputFakePath = document.getElementById("fake-path");
@@ -22,6 +24,21 @@ const downloadYoutube = () => {
   let path = document.getElementById("path").files[0].path;
   
   // 재생 목록에서 각 영상들의 URL을 가져오는 작업 필요
+  // 재생 목록은 window["ytInitialData"] 에 존재
+  request({
+    uri:playListUrl,
+  }, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      const $ = cheerio.load(body);
+
+      // 영상 URL
+      // $('.playlist-video')[i].attribs.href
+      console.log($('.playlist-video')[0].attribs.href);
+
+    } else {
+      console.error(error);
+    }
+  });
 
   return false;
 }
